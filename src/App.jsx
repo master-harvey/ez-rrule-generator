4b94535a48235667e0975ce63a483da13c568169
Master__Harvey <ianpadraicharvey@gmail.com>
2023-04-07 13:17:40 -0600

    basic css in demo now working with import

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,20 @@
 import { useState } from "react";
 import githubLogo from './github.svg'
-import './App.css'
 
 import EzRRule from 'ez-rrule-generator'
 
 export default function App() {
     const [rule, setRule] = useState("")
     return <>
-        <div className="wrapper">
-            <h1>Ez RRule Generator <a href="https://github.com/master-harvey/ez-rrule-generator"><img src={githubLogo} alt="github" width={25} /></a></h1>
-            <EzRRule onChange={(r) => setRule(r)} />
-        </div>
+        <br />
+        <h1 className="centered">Ez RRule Generator</h1>
+        <EzRRule onChange={(r) => setRule(r)} className="wrapper" />
         <p className="centered">Generated RRule sent to your onChange Callback:<br />{rule}</p>
+        <br />
+        <p align="center">
+            <a href="https://github.com/master-harvey/ez-rrule-generator" target="_blank">
+                <img src={githubLogo} alt="github" width={25} />
+            </a>
+        </p>
     </>
 }
